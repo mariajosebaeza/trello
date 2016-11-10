68c37f4802052f92958ba625f99d33b9195e051d
Maria Jose Baeza Sanhueza <mariajosebaezasanhueza@MacBook-Air-de-Maria.local>
2016-11-09 23:15:55 -0300

    codigo js, mas maqueta en html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 
-function nuevaTarea() {
-	var document.getElementById("Nueva Tarea");
-}
+function nuevaTarea(){
 
+    var  contenedor  = document.getElementById('padre'); //el contenedor de todo lo agregado 
+
+    // primero se crean los elementos de la maqueta de html 
+    var row = document.createElement('div');             //Se crea div con clase row
+    var primerDiv = document.createElement('div');      //se crea primer div
+    var primerInput = document.createElement('input'); //se crea primer input
+    var botonuno = document.createElement('button');  //nombre boton
+
+    var nombreBoton = document.createTextNode('Guardar');   // se crea nombre del boton
+
+    //  se agregan  clases y atributos de los elementos creados
+
+    row.classList.add('row');
+    primerDiv.classList.add('col-md-4',"primer-input");
+    primerInput.setAttribute('type','text');
+    primerInput.setAttribute('name','form-control');
+    primerInput.setAttribute('id','filaNueva');
+    primerInput.setAttribute('placeholder','crear fila..');
+    botonuno.classList.add('btn' , 'btn-info' , 'btn-guardar');
+    botonuno.setAttribute('type','button');
+
+    // nombrar las posiciones 
+
+    primerDiv.appendChild(primerInput); // primerDiv es padre de primerInput
+    primerDiv.appendChild(botonuno);    // primer div es padre de botonuno
+    row.appendChild(primerDiv);         // row padre de primer div
+    contenedor.appendChild(row);        // contenedor es padre de row 
+                                        //falta la variable del boton, es padre de la variable de nodo nombre. 
+
+}
